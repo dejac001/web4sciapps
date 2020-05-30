34d8eb3c5a33f1266d8be75b2045bcd3112ba647
Robert DeJaco <dejac001@umn.edu>
2020-05-30 15:17:04 -0400

    Equipping the input page with output results

--- controller.py
+++ controller.py
@@ -9,15 +9,16 @@ class InputForm(Form):
     r = FloatField(validators=[validators.InputRequired()])
 
 # View
-@app.route('/hw1', methods=['GET', 'POST'])
+@app.route('/hw2', methods=['GET', 'POST'])
 def index():
     form = InputForm(request.form)
     if request.method == 'POST' and form.validate():
         r = form.r.data
         s = compute(r)
-        return render_template("view_output.html", form=form, s=s)
     else:
-        return render_template("view_input.html", form=form)
+        s = None
+
+    return render_template("view.html", form=form, s=s)
 
 
 if __name__ == '__main__':
--- hw.py
+++
@@ -1,4 +0,0 @@
-import sys, math
-r = float(sys.argv[1])
-s = math.sin(r)
-print("Hello, world! sin(%g)=%g" % (r, s))
