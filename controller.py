869bfa04c352778fd87174d2593526bd71a83edd
Robert DeJaco <dejac001@umn.edu>
2020-05-30 15:25:32 -0400

    Splitting the app into MVC

--- controller.py
+++ controller.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request
-from wtforms import Form, FloatField, validators
 from compute import compute
+from model import InputForm
 
 app = Flask(__name__)
 
-# Model
-class InputForm(Form):
-    r = FloatField(validators=[validators.InputRequired()])
-
 # View
-@app.route('/hw2', methods=['GET', 'POST'])
+@app.route('/hw3', methods=['GET', 'POST'])
 def index():
     form = InputForm(request.form)
     if request.method == 'POST' and form.validate():
--- model.py
+++ model.py
@@ -1,2 +1,4 @@
-r = 0.0     # input
-s = None    # output
+from wtforms import Form, FloatField, validators
+
+class InputForm(Form):
+    r = FloatField(validators=[validators.InputRequired()])
